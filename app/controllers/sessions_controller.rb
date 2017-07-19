cb0532977cf50e807d40feb36b0815f545417b64
satong <stephanietong@gmail.com>
2017-07-19 12:34:13 -0700

    Add controller and views for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if authenticate
+      redirect_to current_user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    # session.delete(:user_id)
+    # render 'new'
+    log_out
+    redirect_to root_path
+  end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,31 @@
 module SessionsHelper
+  def authenticate
+    user = User.find_by(username: params[:username])
+    if user && user.password == params[:password]
+      log_in(user)
+      return true
+    end
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+    # object if exists, nil if not, !! returns a forced boolean
+  end
+
+  def authorized_user?(item)
+    logged_in? && current_user.id == item.user_id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end

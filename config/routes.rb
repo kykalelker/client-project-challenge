2f808d47a077297472e818ced1a8fbfee9adb088
Marissab13 <marissab13@gmail.com>
2017-07-20 10:33:46 -0700

    create user show page with movies they've added and comments they've made.

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,14 @@
 Rails.application.routes.draw do
   root 'movies#index'
-
-  resources :users, only: [:new, :create, :show]
-
+  resources :users, only: [:new, :create, :show] do
+    resources :favorites, only: [:index, :show, :create]
+    resources :watches, only: [:index, :show, :create]
+  end
   get '/login', to: 'sessions#new', as: 'new_login'
   post '/login', to: 'sessions#create', as: 'login'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
-
   resources :movies, shallow: true do
     resources :comments
   end
-
-  resources :users do
-    resources :favorites, only: [:index]
-  end
-
-  resources :users do
-    resources :watches, only: [:index]
-  end
-
   resources :searches, only: [:index]
-
 end

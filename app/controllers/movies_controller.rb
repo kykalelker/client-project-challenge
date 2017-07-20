9218f837698fd68a2ac897a7170e3827d186bb08
Kanan Kalelker <kykalelker@gmail.com>
2017-07-19 18:37:33 -0700

    homepage - shows all movies and activity feed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,14 @@
 class MoviesController < ApplicationController
+	include MoviesHelper
 
-def index
-	@movies = Movie.all
+	def index
+		@movies = Movie.all
+		@moviesRecent = Movie.recent 
+		@recentMoviesCommented = recentMoviesCommented
+	end
+
+
+	def show
+		@movie = Movie.find(params[:id])
+	end
 end
-
-end
-
-#add activity feed to the homepage view
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,14 @@
 Rails.application.routes.draw do
   root 'movies#index'
-
   resources :users, only: [:new, :create, :show] do
-    resources :favorites, only: [:index, :show]
-    resources :watches, only: [:index, :show]
+    resources :favorites, only: [:index, :show, :create]
+    resources :watches, only: [:index, :show, :create]
   end
-
   get '/login', to: 'sessions#new', as: 'new_login'
   post '/login', to: 'sessions#create', as: 'login'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
-
   resources :movies, shallow: true do
     resources :comments
   end
-
   resources :searches, only: [:index]
-
-  resources :favorites, only: [:create]
-
 end
